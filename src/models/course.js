979f4a7ef657a57de126a688e9135e3b9c2c8c0d
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-31 05:43:52 -0400

    Add Course model with schema definition for university management system

---
+++ src/models/course.js
@@ -0,0 +1,11 @@
+// models/Course.js
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+    name: String,
+    code: String,
+    department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department' },
+    faculty: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Faculty' }]
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
