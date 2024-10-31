ed1727872aee3be5b26d38ff6f6a9ce766adca83
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-31 05:43:55 -0400

    Add Department model with schema definition for university management system

---
+++ src/models/deparment.js
@@ -0,0 +1,11 @@
+// models/Department.js
+const mongoose = require('mongoose');
+
+const DepartmentSchema = new mongoose.Schema({
+    name: String,
+    code: String,
+    faculty: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Faculty' }],
+    courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+});
+
+module.exports = mongoose.model('Department', DepartmentSchema);
