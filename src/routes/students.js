1a35719d85fabbf6b21e143373f3cfb8e67a3a1c
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-31 06:39:40 -0400

    Add token verification middleware to student routes for enhanced security

--- src/routes/students.js
+++ src/routes/students.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 const ensureAuthenticated = require('../middlewares/auth');
 const studentController = require('../controllers/studentController');
+const verifyToken = require('../middlewares/verifyToken');
 
 // Crear estudiante
-router.post('/', ensureAuthenticated, studentController.createStudent);
+router.post('/', ensureAuthenticated, verifyToken, studentController.createStudent);
 
 // Obtener todos los estudiantes
 router.get('/', studentController.getAllStudents);
 
 // Actualizar estudiante
-router.put('/:id', ensureAuthenticated, studentController.updateStudent);
+router.put('/:id', ensureAuthenticated, verifyToken, studentController.updateStudent);
 
 // Eliminar estudiante
-router.delete('/:id', ensureAuthenticated, studentController.deleteStudent);
+router.delete('/:id', ensureAuthenticated, verifyToken, studentController.deleteStudent);
 
 module.exports = router;
